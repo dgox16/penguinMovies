fca950f22851dd0ab666d54d168be220e6a0336c
Diego <dgox16@gmail.com>
2023-06-14 00:32:49 -0600

    Se cambio la ruta

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:4000/api",
-    withCredentials: true,
+	baseURL: "https://apimovies-production.up.railway.app/api",
+	withCredentials: true,
 });
 
 export default instance;
